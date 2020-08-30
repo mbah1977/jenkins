cdb04ea04606182a6023e43568fca0c6babd70c7
root <root@localhost>
2020-08-29 21:17:33 -0400

    k

--- get_token.sh
+++ get_token.sh
@@ -13,4 +13,5 @@ curl --header "X-Vault-Token: $r_t"  --request POST  --data @payload.json  http:
 curl --header "X-Vault-Token: $r_t" --request GET http://127.0.0.1:8200/v1/auth/approle/role/jenkins | jq
 role_id=`curl  --header "X-Vault-Token: $r_t" http://192.168.91.168:8200/v1/auth/approle/role/jenkins/role-id | jq .data.role_id` 
 secret_id=`curl  --header "X-Vault-Token: $r_t"  --request POST    http://192.168.91.168:8200/v1/auth/approle/role/jenkins/secret-id | jq .data.secret_id`
-curl --request POST --data '{"role_id": '$role_id',"secret_id": '$secret_id'}' http://192.168.91.168:8200/v1/auth/approle/login | jq .auth.client_token | cut -d'"' -f2  > /var/lib/jenkins/workspace/jenkins/token
+curl --request POST --data '{"role_id": '$role_id',"secret_id": '$secret_id'}' http://192.168.91.168:8200/v1/auth/approle/login | jq .auth.client_token \
+| cut -d'"' -f2  > /var/lib/jenkins/workspace/jenkins/token
